a5043e240ea120dbd17920f3e8956242e32befca
IAmFuture0 <163365546+IAmFuture0@users.noreply.github.com>
2024-08-07 17:08:12 +0800

    Update matching2D.hpp

--- src/matching2D.hpp
+++ src/matching2D.hpp
@@ -27,4 +27,5 @@ void descKeypoints(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, cv::Mat &
 void matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::KeyPoint> &kPtsRef, cv::Mat &descSource, cv::Mat &descRef,
                       std::vector<cv::DMatch> &matches, std::string descriptorType, std::string matcherType, std::string selectorType);
 
+void detKeypoints(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis, std::string detectorType);
 #endif /* matching2D_hpp */
